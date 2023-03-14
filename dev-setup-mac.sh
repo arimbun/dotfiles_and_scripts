b2f899d8c7640aa533c05b73e6290d5a61d26ce8
Arthur Rimbun <arthur@arimbun.net>
2023-03-14 21:50:04 +1100

    Add markdown-toc to install script

--- dev-setup-mac.sh
+++ dev-setup-mac.sh
@@ -17,6 +17,9 @@ cp .* $HOME
 npm install -g neovim
 python3 -m pip install --user --upgrade pynvim
 
+# Markdown-TOC
+npm install -g markdown-toc
+
 # VimPlug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
